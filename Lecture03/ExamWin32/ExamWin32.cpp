9c700aa121a1d60a821cf670c5a35a48fb84fb55
yeqfx <yeqfx@hotmail.com>
2021-03-15 22:00:25 +0900

    Windows 데스크톱 프로그래밍 Step 3

---
+++ Lecture03/ExamWin32/ExamWin32.cpp
@@ -0,0 +1,66 @@
+﻿// ExamWin32.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ExamWin32.h"
+#include <windowsx.h>
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    if (uMsg == WM_DESTROY) PostQuitMessage(0);
+    else if (uMsg == WM_LBUTTONDOWN) {
+        HDC h_dc = GetDC(hWnd);
+
+        //int x = lParam & 0x0000FFFF;
+        //int y = (lParam >> 16) & 0x0000FFFF;
+
+        //int x = LOWORD(lParam);
+        //int y = HIWORD(lParam);
+
+        int x = GET_X_LPARAM(lParam);
+        int y = GET_Y_LPARAM(lParam);
+
+        if (MK_CONTROL & wParam) Ellipse(h_dc, x - 15, y - 15, x + 15, y + 15);
+        else Rectangle(h_dc, x - 15, y - 15, x + 15, y + 15);
+
+        ReleaseDC(hWnd, h_dc);
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASS wc;
+
+    wchar_t my_class_name[] = L"tipssoft";
+    wc.cbClsExtra = NULL;
+    wc.cbWndExtra = NULL;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hInstance = hInstance;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszClassName = my_class_name;
+    wc.lpszMenuName = NULL;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+
+    RegisterClass(&wc);
+
+    HWND hWnd = CreateWindowW(my_class_name, L"www.tipssoft.com", WS_OVERLAPPEDWINDOW, 100, 90, 400, 350, NULL, NULL, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
