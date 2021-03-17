73c58211e9bd148e17c9724725a64369ddaec5e6
yeqfx <yeqfx@hotmail.com>
2021-03-17 12:50:08 +0900

    8강 Windows 데스크톱 응용 프로그램 Step 8   - DC 생성 : CreateCompatibleDC   - 윈도우 위치 구하기   - WM_PAINT 강제 완료 : ValidateRect   - MessageBox

---
+++ Lecture08/ExamWin32/ExamWin32.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "ExamWin32.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    if (uMsg == WM_DESTROY) PostQuitMessage(0);
+    else if (uMsg == WM_CLOSE) {
+        int check = MessageBox(hWnd, L"프로그램을 종료하시겠습니까?", L"www.tipssoft.com 종료 확인!", MB_ICONQUESTION | MB_OKCANCEL);
+        if (IDCANCEL == check) return 0;
+    }    
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASS wc;
+
+    wchar_t my_class_name[] = L"tipssoft";
+    wc.cbClsExtra = NULL;
+    wc.cbWndExtra = NULL;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hInstance = hInstance;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszClassName = my_class_name;
+    wc.lpszMenuName = NULL;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+
+    RegisterClass(&wc);
+
+    HWND hWnd = CreateWindowW(my_class_name, L"www.tipssoft.com", WS_OVERLAPPEDWINDOW, 100, 90, 400, 350, NULL, NULL, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
